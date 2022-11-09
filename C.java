e230aeadf3c3f69b20dee88e0e70b063a2d69345
handa-kun <Whitey.A@yandex.ru>
2022-11-09 16:50:29 +0300

    r0

---
+++ C.java
@@ -0,0 +1,14 @@
+public class C {
+
+    private String c = "test";
+
+    private String f = "init";
+
+    public byte oo() {
+        return 1;
+    }
+
+    public int[] ii() {
+        return new int[]{0, 1, 2, 3, 4};
+    }
+}
---
+++ D.java
@@ -0,0 +1,6 @@
+public interface D {
+
+    int af();
+
+    String kk();
+}
